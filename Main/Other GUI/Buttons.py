38248034c566c25cfb484184ee9fd128960acc69
Cole Sylvester <colesly15@gmail.com>
2023-05-02 18:11:30 -0500

    Added button function plus RPi stuff on GUI

--- Main/Other GUI/Buttons.py
+++ Main/Other GUI/Buttons.py
@@ -1,35 +1,39 @@
 import RPi.GPIO as GPIO
-from time import sleep
-buttons = [17, 16, 13, 12, 6, 5, 4, 27, 26, 25, 24, 23, 22, 21, 20, 19]
 
-button_vals = {17: "1",
-               16: "2",
-               13: "3",
-               6: "4",
-               5:"5",
-               4: "6",
-               26: "7",
-               25: "8",
-               24: "9",
-               22: "<",
-               21: "0",
-               20: ">",
-               27: "Run",
-               12: "Lock",
-               23: "Read",
-               19: "Dist"}
+def buttons():
 
-GPIO.setmode(GPIO.BCM)
+    button_vals = {17: "1",
+                16: "2",
+                13: "3",
+                6: "4",
+                5:"5",
+                4: "6",
+                26: "7",
+                25: "8",
+                24: "9",
+                22: "<",
+                21: "0",
+                20: ">",
+                27: "Run",
+                12: "Lock",
+                23: "Read",
+                19: "Dist"}
 
-GPIO.setup(buttons, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    argument_list = []
 
-while True:
-    
-    for i in buttons:
+    RUNNING = True
+
+    while RUNNING:
         
-        if GPIO.input(i) == True:
+        for i in buttons:
             
-            print(button_vals[i])
-            sleep(0.2)
-            
-            
+            if GPIO.input(i) == True:
+                
+                if i == 12:
+
+                    RUNNING = False
+                    return argument_list
+                
+                else:
+
+                    argument_list.append(button_vals[i])
