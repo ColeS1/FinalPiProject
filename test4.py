e892cef8472f0530ac2312170bf1f481a1e324da
Hazem <hazimoto1172@gmail.com>
2023-05-02 22:26:56 -0500

    adjusted radio file

--- test4.py
+++
@@ -1,8 +0,0 @@
-output = {'f': '6', 'r': '99'}
-
-for key in output:
-    output[key] = (output[key], 'new_element')
-    
-print(output)
-
-# OUTPUT: {'f': ('6', 'new_element'), 'r': ('99', 'new_element')}
